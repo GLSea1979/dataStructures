36d5d326f7e8cf9d9d60c232c64b6a0bbb4baeec
GLSea1979 <builtbygl@gmail.com>
2017-04-11 13:36:24 -0700

    started string man

---
+++ strings.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const oneString = "Strings are fun, strings are cool!";
+const stringTwo = "Let us learn while we can.";
+
+console.log(oneString.charAt(1));
+// should be S
+console.log(oneString.charCodeAt(1));
+// should be a unicode
+console.log(oneString.concat(stringTwo));
