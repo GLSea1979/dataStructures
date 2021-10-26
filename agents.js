bc2febfb1bfa1d0d613d4f8a0dd0f27cb063e159
Gary Lundgren <builtbygl@gmail.com>
2021-10-25 23:43:33 -0700

    agents

---
+++ agents.js
@@ -0,0 +1,14 @@
+const agentList = [
+    {
+        createdAt : '20211001',
+        expiresAt : '20211111'
+    },
+    {
+        createdAt : '20211101',
+        expiresAt : '20211112'
+    }
+]
+
+/* 
+   Write a function that looks over an agent list similar to above and returns a true/false for whether the agents time period ever overlaps.
+*/
