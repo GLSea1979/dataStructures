a9b1fb99cb0bce025bc0d02b3d78373f141777d5
Gary Lundgren <builtbygl@gmail.com>
2021-10-28 22:19:51 -0700

    update to let

--- merge-sort.js
+++ merge-sort.js
@@ -1,19 +1,19 @@
-var myArr = [22, 11, 45, 1, 2, 5, 6, 99, 300, 46, 78, 79, 23, 44, 12, 7, 8, 10, 3];
-var smallArr = [4, 2, 3, 1];
+let myArr = [22, 11, 45, 1, 2, 5, 6, 99, 300, 46, 78, 79, 23, 44, 12, 7, 8, 10, 3];
+let smallArr = [4, 2, 3, 1];
 console.log(myArr, 'beginning array')
 
 function mergeSort(arr) {
   if(arr.length < 2) return arr;
 
-  var middle = (arr.length / 2);
-  var left = arr.slice(0, middle);
-  var right = arr.slice(middle, arr.length);
+  let middle = (arr.length / 2);
+  let left = arr.slice(0, middle);
+  let right = arr.slice(middle, arr.length);
 
   return merge(mergeSort(left), mergeSort(right));
 }
 
 function merge(left, right) {
-  var result = [];
+  let result = [];
 
   while(left.length && right.length) {
     if(left[0] <= right[0]) {
